757339167d691b2760464af9ceaef45bc623aab3
Aaron <aaronliu12@gmail.com>
2017-02-14 14:40:25 +0800

    updated

---
+++ dist/js/address.js
@@ -0,0 +1,17 @@
+ function shanghai() {
+            document.getElementById("shanghai").style.display = "block";
+            document.getElementById("suzhou").style.display = "none";
+            document.getElementById("hangzhou").style.display = "none";
+}
+
+ function hangzhou() {
+            document.getElementById("shanghai").style.display = "none";
+            document.getElementById("suzhou").style.display = "none";
+            document.getElementById("hangzhou").style.display = "block";
+}
+
+ function suzhou() {
+            document.getElementById("shanghai").style.display = "none";
+            document.getElementById("suzhou").style.display = "block";
+            document.getElementById("hangzhou").style.display = "none";
+}
