56f71d04a08cc51bade1e7460314d89c962a9fd0
Aaron <aaronliu12@gmail.com>
2017-02-14 15:46:55 +0800

    updated

--- dist/js/address.js
+++ dist/js/address.js
@@ -1,17 +1,35 @@
- function shanghai() {
+ function shanghai(object) {
             document.getElementById("shanghai").style.display = "block";
             document.getElementById("suzhou").style.display = "none";
             document.getElementById("hangzhou").style.display = "none";
+            document.getElementById("hangzhoubutton").classList.remove("active");
+            document.getElementById("suzhoubutton").classList.remove("active");
+        if(object.classList[2] !== "active"){
+            
+            object.classList.add('active');
+        }
+            
+            
+        
+            
 }
 
- function hangzhou() {
+ function hangzhou(object) {
             document.getElementById("shanghai").style.display = "none";
             document.getElementById("suzhou").style.display = "none";
+            document.getElementById("shanghaibutton").classList.remove('active');
+            document.getElementById("suzhoubutton").classList.remove('active');
             document.getElementById("hangzhou").style.display = "block";
+            object.classList.add("active");
+     
+     
 }
 
- function suzhou() {
+ function suzhou(object) {
             document.getElementById("shanghai").style.display = "none";
             document.getElementById("suzhou").style.display = "block";
             document.getElementById("hangzhou").style.display = "none";
+            document.getElementById("shanghaibutton").classList.remove('active');
+            document.getElementById("hangzhoubutton").classList.remove('active');
+            object.classList.add("active");
 }
