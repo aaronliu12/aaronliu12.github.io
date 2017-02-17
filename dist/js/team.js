1638ccb527cb97f7fa8010913d7ae70d2cb62303
Aaron <aaronliu12@gmail.com>
2017-02-17 10:59:45 +0800

    upgrade

---
+++ dist/js/team.js
@@ -0,0 +1,11 @@
+/*$( "#ECs" ).mouseover(function() {
+  $("#paul").css('filter', 'grayscale(0%)');
+    $("#chip").css('filter', 'grayscale(0%)');
+    $("#EC").css('color', 'black');
+});
+
+$( "#ECs" ).mouseout(function() {
+  $("#paul").css('filter', 'grayscale(100%)');
+    $("#chip").css('filter', 'grayscale(100%)');
+    $("#EC").css('color', 'white');
+});*/
